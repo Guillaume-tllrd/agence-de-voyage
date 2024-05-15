ba2c03a9784985a0bafefa4ae05fcd4f2b22aef7
Guillaume-tllrd <guillaumetillard10@gmail.com>
2024-05-15 16:38:23 +0200

    maj mediaqueries

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// -------------NAVBAR------
+const nav = document.querySelector(".nav");
+const icons= document.getElementById("icons");
+icons.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+
 //-------------------Sport carte-------------------
 // Base jump
 const baseJumpInfo = document.getElementById("base-jump-info");
@@ -5,6 +12,7 @@ const carteInfosBaseJump = document.querySelector(".carte-infos-basejump");
 
 baseJumpInfo.addEventListener("click", () => {
     carteInfosBaseJump.style.opacity = "1";
+    carteInfosBaseJump.style.zIndex = "1";
 })
 document.addEventListener("click", (event) => {
     if (!baseJumpInfo.contains(event.target)) {
@@ -18,6 +26,7 @@ const carteInfosParapente = document.querySelector(".carte-infos-parapente");
 
 parapenteInfo.addEventListener("click", () => {
     carteInfosParapente.style.opacity = "1";
+    carteInfosParapente.style.zIndex = "1";
 })
 document.addEventListener("click", (event) => {
     if (!parapenteInfo.contains(event.target)) {
@@ -31,6 +40,7 @@ const carteInfosSaut = document.querySelector(".carte-infos-saut");
 
 sautElastiqueInfo.addEventListener("click", () => {
     carteInfosSaut.style.opacity = "1";
+    carteInfosSaut.style.zIndex = "1";
 })
 document.addEventListener("click", (event) => {
     if (!sautElastiqueInfo.contains(event.target)) {
@@ -44,6 +54,7 @@ const carteInfosRafting = document.querySelector(".carte-infos-rafting");
 
 raftingInfo.addEventListener("click", () => {
     carteInfosRafting.style.opacity = "1";
+    carteInfosRafting.style.zIndex = "1";
 })
 document.addEventListener("click", (event) => {
     if (!raftingInfo.contains(event.target)) {
@@ -57,6 +68,7 @@ const carteInfosEscalade = document.querySelector(".carte-infos-escalade");
 
 escaladeInfo.addEventListener("click", () => {
     carteInfosEscalade.style.opacity = "1";
+    carteInfosEscalade.style.zIndex = "1";
 })
 document.addEventListener("click", (event) => {
     if (!escaladeInfo.contains(event.target)) {
@@ -70,6 +82,7 @@ const carteInfosVtt = document.querySelector(".carte-infos-vtt");
 
 vttInfo.addEventListener("click", () => {
     carteInfosVtt.style.opacity = "1";
+    carteInfosVtt.style.zIndex = "1";
 })
 document.addEventListener("click", (event) => {
     if (!vttInfo.contains(event.target)) {
@@ -99,3 +112,53 @@ buttons.forEach((button) => {
     });
   });
 
+  const buttons2 = document.querySelectorAll(".btn-caroussel2");
+  const slides2 = document.querySelectorAll(".slide2");
+  buttons2.forEach((button) => {
+      
+      button.addEventListener("click", (e) => {
+        const calcNextSlide2 = e.target.id === "next2" ? 1 : -1;
+        
+        const slideActive2 = document.querySelector(".active2");
+        newIndex2 = calcNextSlide2 + [...slides2].indexOf(slideActive2);
+        if (newIndex2 < 0) newIndex2 = [...slides2].length - 1;
+    
+        if (newIndex2 >= [...slides2].length) newIndex2 = 0;
+        slides2[newIndex2].classList.add("active2");
+        slideActive2.classList.remove("active2");
+      });
+    });
+
+const buttons3 = document.querySelectorAll(".btn-caroussel3");
+const slides3 = document.querySelectorAll(".slide3");
+buttons3.forEach((button) => {
+    
+    button.addEventListener("click", (e) => {
+      const calcNextSlide3 = e.target.id === "next3" ? 1 : -1;
+      
+      const slideActive3 = document.querySelector(".active3");
+      newIndex3 = calcNextSlide3 + [...slides3].indexOf(slideActive3);
+      if (newIndex3 < 0) newIndex3 = [...slides3].length - 1;
+  
+      if (newIndex3 >= [...slides3].length) newIndex3 = 0;
+      slides3[newIndex3].classList.add("active3");
+      slideActive3.classList.remove("active3");
+    });
+  });
+
+const buttons4 = document.querySelectorAll(".btn-caroussel4");
+const slides4 = document.querySelectorAll(".slide4");
+buttons4.forEach((button) => {
+    
+    button.addEventListener("click", (e) => {
+      const calcNextSlide4 = e.target.id === "next4" ? 1 : -1;
+      
+      const slideActive4 = document.querySelector(".active4");
+      newIndex4 = calcNextSlide4 + [...slides4].indexOf(slideActive4);
+      if (newIndex4 < 0) newIndex4 = [...slides4].length - 1;
+  
+      if (newIndex4 >= [...slides4].length) newIndex4 = 0;
+      slides4[newIndex4].classList.add("active4");
+      slideActive4.classList.remove("active4");
+    });
+  });
